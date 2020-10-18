80ff281f85562fc86094f81a37ba97e6d3484570
Matthew Wildermuth <17675296+mgwildermuth@users.noreply.github.com>
2020-10-18 16:08:42 -0400

    Created some structure of the message.
    getMessage() not finished. values should be floats, will have to figure that out.

--- server/Message.java
+++ server/Message.java
@@ -6,20 +6,55 @@
  * @author Adam J. Lee (adamlee@cs.pitt.edu) 
  *
  */
-public class Message implements java.io.Serializable
-{
-    private static final long serialVersionUID = 3452098893656819551L;
-    /** The text string encoded in this Message object */
-    public String theMessage;
+import java.math.BigInteger;
 
-    /**
-     * Constructor.
-     *
-     * @param _msg The string to be encoded in this Message object
-     *
-     */
-    public Message(String _msg){
-	theMessage = _msg;
-    }
+public class Message //implements java.io.Serializable
+{
+	//private static final long serialVersionUID = 3452098893656819551L;
+	/** The text string encoded in this Message object */
+	public String theMessage;
+	private final int packetLen = 32;
+	private final int messageIdLen = 32;
+	private BigInteger messageId;
+	private BigInteger value;
+	private BigInteger value2;
+	
+	/**
+	* Constructor.
+	*
+	* @param _msg The string to be encoded in this Message object
+	*
+	*/
+	public Message(int _msgid)
+	{
+		setMsgId(_msgid);
+	}
+	
+	public void setValue(int val)
+	{
+		value = new BigInteger(String.valueOf(val));
+	}
+	
+	public void setValue2(int val)
+	{
+		value2 = new BigInteger(String.valueOf(val));
+	}
+	
+	public void setMsgId(int id)
+	{
+		messageId = new BigInteger(String.valueOf(id));
+	}
+	
+	public byte[] getMessage()
+	{
+		byte[] retval = new byte[16];
+		byte[] tempm = messageId.toByteArray();
+		retval[0] = tempm[0];
+		retval[1] = tempm[1];
+		retval[2] = tempm[2];
+		retval[3] = tempm[3];
+		
+		return retval;
+	}
 
 }  //-- End class Message
