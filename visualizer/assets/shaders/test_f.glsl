0ab1f7e34f0d87b4c00560f8b18ddd80b0058172
sona-padin <sonap@DESKTOP-UOGLHVF.localdomain>
2021-02-08 00:35:20 -0500

    Added 'flame' mesh and a method to make flame mesh invisible

--- visualizer/assets/shaders/test_f.glsl
+++ visualizer/assets/shaders/test_f.glsl
@@ -7,7 +7,7 @@ out vec4 color;
 
 void main()
 {
-    float i = exp(dot(normalize(vec3(0,0,0) - vec3(3,2,3)), normalize(n))) / exp(1.0);
-    color = vec4(i, i, i + 0.05, 1.0);
+    //float i = exp(dot(normalize(vec3(0,0,0) - vec3(3,2,3)), normalize(n))) / exp(1.0);
+    color = vec4(1, 0.5, 0, 1.0);
 }
 
